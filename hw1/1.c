fa019ced2d3d0bdde584939fad17370e40f08d97
NTUB05902020 <b05902020@csie.ntu.edu.tw>
2019-03-07 16:38:56 +0800

    2019/03/07 16:38

--- hw1/1.c
+++ hw1/1.c
@@ -5,12 +5,17 @@
 #include <netinet/ip.h>
 #include <errno.h>
 
-int sockfd, ttl = 60;
+int sockfd, ttl;
+
+void changeTTL(int newValue){
+	ttl = newValue;
+	int ret = setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
+	printf("setsockopt = %d\n", ret);
+}
 
 int main(int argc, char **argv){
 	sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-	//printf("sockfd = %d\n", sockfd);
-	
-	setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
+	printf("sockfd = %d\n", sockfd);
+	changeTTL(60);
 	exit(0);
 }
