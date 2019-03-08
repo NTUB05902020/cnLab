2b04453d46061abfe49a7ebfc587a1ad053cc29a
張睿傑 <juichieh.chang@gmail.com>
2019-03-08 15:16:09 +0800

    better

--- hw1/icmp.c
+++ hw1/icmp.c
@@ -1,22 +1,35 @@
 #include "icmp.h"
 #include <stdlib.h>
 #include <stdio.h>
-void set_checksum(SPac *pac){
-  unsigned int sum = 0;
+#include <netinet/in.h>
+void set_checksum(ICMP *pac){
+  uint32_t sum = 0;
   sum = pac->type;
   sum <<= 8;
   sum += pac->code + pac->identifier + pac->seqNum;
-  pac->checksum = ~(sum + (sum>>16));
-}  
-Info *get_Info(RPac *pac){
-  // echo reply
-  if(pac->type == 0){
+  pac->checksum = (uint16_t)~((sum & 0xffff) + (sum>>16));
+}
+Info *get_info(char *buff){
+  // IPv4 VHL = version(4 bits) + header length(4 bits)
+  vhl = *(uint8_t *)buff;
+  header_size = (vhl & 0x0f) * 4;
+
+  // get the IP address of the source
+  // source address starts at the 12th byte of the IP header
+  uint32_t IP = *(uint8_t *)(buff + 12);
+
+  // get the ICMP header
+  ICMP *icmp_head = (ICMP *)(buff + header_size);
+
+  // check type
+  if(icmp_head->type == 0){
+    // echo reply
     Info *info = (Info *)malloc(sizeof(Info));
-    info->IP = pac->IP;
-    info->identifier = pac->original.identifier;
-    info->seqNum = pac->original.seqNum;
+    info->IP = IP;
+    info->identifier = ntohs(icmp_head->identifier);
+    info->seqNum = ntohs(icmp_head->seqNum);
     return info;
   }
-  # invalid
+  // invalid
   return NULL;
 }
--- hw1/icmp.h
+++ hw1/icmp.h
@@ -1,24 +1,22 @@
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+// ICMP header structure
 typedef struct{
-  unsigned char type; // unsigned 8bits
-  unsigned char code; // unsigned 8bits
-  unsigned short checksum; // unsigned 16bits
-  unsigned short identifier; // unsigned 16bits
-  short seqNum; // signed 16bits
-}SPac;
+  uint8_t type; // unsigned 8bits
+  uint8_t code; // unsigned 8bits
+  uint16_t checksum; // unsigned 16bits
+  uint16_t identifier; // unsigned 16bits
+  uint16_t seqNum; // unsigned 16bits
+}ICMP;
 
 typedef struct{
-  unsigned char type; // unsigned 8bits
-  unsigned char code; // unsigned 8bits
-  int others; // 32bits
-  int IP; // 32bits
-  Spac original; // 64bits
-}RPac;
-
-typedef struct{
-  unsigned char IP[4];
-  unsigned short identifier;
-  short seqNum;
+  uint32_t IP;
+  uint16_t identifier;
+  uint16_t seqNum;
 }Info;
-
-void set_checksum(SPac *pac);
-Info *get_Info(RPac *pac);
+// set the checksum field of the given package "pac"
+void set_checksum(ICMP *pac);
+// return IP, identifier and seqNum of the given package "buff"
+Info *get_info(char *buff);
